0107ba89b95eca6d5602d9a5113991991a5def47
myuon <ioi.joi.koi.loi@gmail.com>
2019-11-07 00:56:25 +0900

    fix: transfer QueryBuilder ownership to QueryExecutor

--- src/query.rs
+++ src/query.rs
@@ -77,34 +77,34 @@ impl QueryBuilder {
         .join(" ")
     }
 
-    pub async fn load<R: QueryExecutor<T, E>, T, E>(&self, executor: &R) -> Result<Vec<T>, E> {
-        executor.load(self.build()).await
+    pub async fn load<R: QueryExecutor<T, E>, T, E>(self, executor: &R) -> Result<Vec<T>, E> {
+        executor.load(self).await
     }
 
-    pub async fn first<R: QueryExecutor<T, E>, T, E>(&self, executor: &R) -> Result<T, E> {
-        executor.first(self.build()).await
+    pub async fn first<R: QueryExecutor<T, E>, T, E>(self, executor: &R) -> Result<T, E> {
+        executor.first(self).await
     }
 
-    pub async fn execute<R: QueryExecutor<T, E>, T, E>(&self, executor: &R) -> Result<u64, E> {
-        executor.execute(self.build()).await
+    pub async fn execute<R: QueryExecutor<T, E>, T, E>(self, executor: &R) -> Result<u64, E> {
+        executor.execute(self).await
     }
 
-    pub async fn save<R: QueryExecutor<T, E>, T, E>(&self, executor: &R) -> Result<(), E> {
-        executor.save(self.build()).await
+    pub async fn save<R: QueryExecutor<T, E>, T, E>(self, executor: &R) -> Result<(), E> {
+        executor.save(self).await
     }
 
-    pub async fn save_all<R: QueryExecutor<T, E>, T, E>(&self, executor: &R) -> Result<(), E> {
-        executor.save_all(self.build()).await
+    pub async fn save_all<R: QueryExecutor<T, E>, T, E>(self, executor: &R) -> Result<(), E> {
+        executor.save_all(self).await
     }
 }
 
 #[async_trait]
 pub trait QueryExecutor<T, E> {
-    async fn load(&self, _: String) -> Result<Vec<T>, E>;
-    async fn first(&self, _: String) -> Result<T, E>;
-    async fn execute(&self, _: String) -> Result<u64, E>;
-    async fn save(&self, _: String) -> Result<(), E>;
-    async fn save_all(&self, _: String) -> Result<(), E>;
+    async fn load(&self, builder: QueryBuilder) -> Result<Vec<T>, E>;
+    async fn first(&self, builder: QueryBuilder) -> Result<T, E>;
+    async fn execute(&self, builder: QueryBuilder) -> Result<u64, E>;
+    async fn save(&self, builder: QueryBuilder) -> Result<(), E>;
+    async fn save_all(&self, builder: QueryBuilder) -> Result<(), E>;
 }
 
 #[test]
