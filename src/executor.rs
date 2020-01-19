8fd908c610f6519b6ac7c9cda0a9145a64a115cd
myuon <ioi.joi.koi.loi@gmail.com>
2020-01-19 20:01:11 +0900

    fix: make QueryExecutor public

--- src/executor.rs
+++ src/executor.rs
@@ -8,8 +8,11 @@ pub trait HasNotFound {
     fn not_found() -> Self;
 }
 
+// async_trait does not support pub trait?
+pub trait QueryExecutor: QueryExecutorImpl {}
+
 #[async_trait]
-pub trait QueryExecutor {
+pub trait QueryExecutorImpl {
     type Error: HasNotFound;
 
     async fn sql_exec<V: Sync + Send>(
