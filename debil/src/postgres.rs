9f37fbdcecff4b158f15832fd7f51ccb85bb96ec
myuon <ioi.joi.koi.loi@gmail.com>
2022-05-03 12:30:43 +0900

    feat: add Executor

--- debil/src/postgres.rs
+++ debil/src/postgres.rs
@@ -1,3 +1,12 @@
+use std::collections::HashMap;
+
+use async_trait::async_trait;
+use sqlx::{
+    encode::IsNull,
+    postgres::{PgArgumentBuffer, PgTypeInfo},
+    Encode, PgPool, Postgres, Type,
+};
+
 pub enum PgType {
     Bool,
     Int,
@@ -22,6 +31,40 @@ impl PgType {
             PgType::Text => "TEXT".to_string(),
         }
     }
+}
+
+pub enum PgValue {
+    Bool(bool),
+    Int(i32),
+    BigInt(i64),
+    VarChar(String),
+    Text(String),
+}
+
+impl<'q> Encode<'q, Postgres> for PgValue {
+    fn encode_by_ref(&self, buf: &mut PgArgumentBuffer) -> IsNull {
+        match self {
+            PgValue::Bool(b) => b.encode(buf),
+            PgValue::Int(i) => i.encode(buf),
+            PgValue::BigInt(i) => i.encode(buf),
+            PgValue::VarChar(s) => s.encode(buf),
+            PgValue::Text(t) => t.encode(buf),
+        }
+    }
+}
+
+impl Type<Postgres> for PgValue {
+    fn type_info() -> PgTypeInfo {
+        PgTypeInfo::with_name("")
+    }
+}
+
+pub trait FromValues {
+    fn from_values(values: HashMap<String, PgValue>) -> Self;
+}
+
+pub trait IntoValues {
+    fn into_map_values(self) -> HashMap<String, PgValue>;
 }
 
 pub trait PgTypeSelector {
@@ -54,6 +97,21 @@ pub trait PgTable {
     fn schema_of(_: std::marker::PhantomData<Self>) -> Vec<(&'static str, PgType, FieldAttribute)>;
 }
 
+pub fn table_name<T: PgTable>() -> &'static str {
+    T::table_name(std::marker::PhantomData::<T>)
+}
+
+pub fn schema_of<T: PgTable>() -> Vec<(&'static str, PgType, FieldAttribute)> {
+    T::schema_of(std::marker::PhantomData::<T>)
+}
+
+pub fn column_names<T: PgTable>() -> Vec<&'static str> {
+    T::schema_of(std::marker::PhantomData::<T>)
+        .into_iter()
+        .map(|(name, _, _)| name)
+        .collect()
+}
+
 pub fn create_table_query<T: PgTable>() -> String {
     format!(
         "CREATE TABLE IF NOT EXISTS {} ({})",
@@ -71,4 +129,50 @@ pub fn drop_table_query<T: PgTable>() -> String {
         "DROP TABLE IF EXISTS {}",
         PgTable::table_name(std::marker::PhantomData::<T>),
     )
+}
+
+pub struct Partial<T> {
+    data: T,
+    columns: Vec<&'static str>,
+}
+
+impl<T: PgTable> Partial<T> {
+    pub fn full(data: T) -> Partial<T> {
+        Partial {
+            data,
+            columns: column_names::<T>(),
+        }
+    }
+}
+
+pub struct QueryResult<T> {
+    pub data: T,
+    pub rows_affected: i64,
+}
+
+#[async_trait]
+pub trait Executor {
+    async fn execute_sql(&self, query: &str) -> Result<QueryResult<()>, sqlx::Error>;
+
+    async fn create<T: PgTable + IntoValues + Send>(
+        &self,
+        item: T,
+    ) -> Result<QueryResult<()>, sqlx::Error>;
+
+    async fn update<T: PgTable + IntoValues + Send>(
+        &self,
+        item: Partial<T>,
+    ) -> Result<QueryResult<()>, sqlx::Error>;
+
+    async fn save<T: PgTable + IntoValues + Send + Sync + Clone>(
+        &self,
+        item: T,
+    ) -> Result<QueryResult<()>, sqlx::Error> {
+        let mut result = self.update(Partial::full(item.clone())).await?;
+        if result.rows_affected == 0 {
+            result = self.create(item).await?;
+        }
+
+        Ok(result)
+    }
 }
